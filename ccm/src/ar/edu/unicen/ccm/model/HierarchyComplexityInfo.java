6350621c10c5b94ab1b17f4c93d48dc9052b43db
pablo.polvorin <pablo.polvorin@gmail.com>
2012-03-24 02:58:13 +0000

    Output the expression used to calculate thehierarchy's weight to the CSV file

--- ccm/src/ar/edu/unicen/ccm/model/HierarchyComplexityInfo.java
+++ ccm/src/ar/edu/unicen/ccm/model/HierarchyComplexityInfo.java
@@ -8,11 +8,13 @@ public class HierarchyComplexityInfo {
 	int depth = 0;
 	int classes = 0;
 	BigInteger cost =BigInteger.valueOf(0);
+	String expr;
 	
-	public HierarchyComplexityInfo(BigInteger cost, int depth, int classes) {
+	public HierarchyComplexityInfo(BigInteger cost, int depth, int classes, String expr) {
 		this.cost = cost;
 		this.depth = depth;
 		this.classes = classes;
+		this.expr = expr;
 	}
 	
 	void addClassCost(BigInteger c) {
@@ -32,6 +34,10 @@ public class HierarchyComplexityInfo {
 	}
 	public int getDepth() {
 		return depth;
+	}
+	
+	public String getExpr() {
+		return expr;
 	}
 	
 	
